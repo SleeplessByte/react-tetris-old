ba3a24ab19393cf8e1d8a5a8cc6a51869fce3b20
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2017-04-08 23:25:00 +0200

    Fix empty dispatch

--- src/store/hold/actions.ts
+++ src/store/hold/actions.ts
@@ -2,7 +2,7 @@ import { StoreThunkAction } from 'store/state'
 import Tetromino from 'interfaces/Tetromino'
 export const ACTION_SWITCH_HELD = 'ACTION_SWITCH_HELD'
 
-import { respawn, ACTION_UPDATE_GHOST } from 'store/game/actions'
+import { respawn } from 'store/game/actions'
 
 export type SwitchHeldAction = { type: typeof ACTION_SWITCH_HELD, payload: Tetromino | null }
 export type HoldAction = SwitchHeldAction
@@ -22,7 +22,6 @@ export function hold(): StoreThunkAction<void> {
     if (!state.hold.locked) {
       dispatch(createSwitchHeldAction(active))
       dispatch(respawn(held))
-      dispatch()
     }
   }
 }
